764b09f97888b4670ff4b95e24c2b881aacd7e89
khangtran97 <khangtdk0612@gmail.com>
2020-06-28 09:08:01 +0700

    ABC

---
+++ src/components/Persons/Person/Person.js
@@ -0,0 +1,49 @@
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+import Aux from '../../../hoc/Auxiliary'
+import classes from './Person.css';
+import withClass from '../../../hoc/withClass';
+import AuthContext from '../../../context/auth-context';
+
+class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
+  static contextType = AuthContext;
+
+  componentDidMount() {
+    this.inputElementRef.current.focus();
+    console.log(this.context.authenticated);
+  }
+
+
+  render() {
+    return (
+      <Aux>
+        {this.context.authenticated ? <p>Authenticated</p> : <p>Please log in</p>}
+        <p onClick={this.props.click}>
+          I'm {this.props.name} and I am {this.props.age} years old!
+        </p>
+        <p>{this.props.children}</p>
+        <input
+          type="text"
+          ref={this.inputElementRef}
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </Aux>
+    );
+  }
+};
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
+
+export default withClass(Person, classes.Person);
